456cab0566bb30d1cfafc112eccb9e74b773f48f
ArmenG <armeng2007@gmail.com>
2022-07-02 11:11:37 -0700

    Making the keyboard show on a lcd screen

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,28 @@
 #include <IRremote.hpp>
 #include <EEPROM.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x27,20,4);
 
 
 int RECV_PIN = 11;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
-
+String input = "";
+String key = "";
 void setup()
 {
   
     Serial.begin(9600);
+    lcd.begin();
+    lcd.backlight();
     pinMode(2, OUTPUT);  
     irrecv.enableIRIn();
     int bytes_empty = 0;
     int x = 0;
+    lcd.setCursor(0,0);
+    lcd.print("booting...");
     for (int i=0; i < 1024; i++)
     { 
       if (EEPROM.read(i) == 255)
@@ -48,15 +56,32 @@ void setup()
    Serial.print("used out of 1024. ");
    Serial.print(bytes_empty);
    Serial.print(" bytes left");
-   
-   
+   lcd.setCursor(0,3);
+   lcd.print(1024-bytes_empty);
+   lcd.print("/1024B ");
+   lcd.setCursor(0,0);
+   lcd.print("          ");
 }
+   
 
 void loop()
 {
+  
   if (irrecv.decode(&results))
   {
-    ReadKeyboard(results.value);
+    key = ReadKeyboard(results.value);
+    if (key == "<ENTER>")
+    {
+      input = "";
+      lcd.setCursor(0,0);
+      lcd.print("                        ");
+    }
+    else
+    {
+      input += key;
+    }
+    lcd.setCursor(0,0);
+    lcd.print(input);
     //Serial.println(results.value);
     irrecv.resume(); // Receive the next value
     digitalWrite(2, HIGH); 
@@ -90,123 +115,124 @@ void Erase()
     EEPROM.write(i, 255);
   }
 }
-void ReadKeyboard(int result)
+String ReadKeyboard(int result)
 {
   if (result == -9691)
   {
-    Serial.print("a");
+    return "a";
   }
   else if (result == -20911)
   {
-    Serial.print("b");
+    return "b";
   }
   else if (result == 30345)
   {
-    Serial.print("c");
+    return "c";
   }
   else if (result == -17851)
   {
-    Serial.print("d");
+    return "d";
   }
   else if (result == 24735)
   {
-    Serial.print("e");
+    return "e";
   }
   else if (result == 31365)
   {
-    Serial.print("f");
+    return "f";
   }
   else if (result == -1531)
   {
-    Serial.print("g");
+    return "g";
   }
   else if (result == 5865)
   {
-    Serial.print("h");
+    return "h";
   }
   else if (result == -5611)
   {
-    Serial.print("i");
+    return "i";
   }
   else if (result == -27031)
   {
-    Serial.print("j");
+    return "j";
   }
   else if (result == 22185)
   {
-    Serial.print("k");
+    return "k";
   }
   else if (result == -10711)
   {
-    Serial.print("l");
+    return "l";
   }
   else if (result == 7905)
   {
-    Serial.print("m");
+    return "m";
   }
   else if (result == 28305)
   {
-    Serial.print("n");
+    return "n";
   }
   else if (result == 6885)
   {
-    Serial.print("o");
+    return "o";
   }
   else if (result == -26011)
   {
-    Serial.print("p");
+    return "p";
   }
   else if (result == 8415)
   {
-    Serial.print("q");
+    return "q";
   }
   else if (result == 12495)
   {
-    Serial.print("r");
+    return "r";
   }
   else if (result == 15045)
   {
-    Serial.print("s");
+    return "s";
   }
   else if (result == -20401)
   {
-    Serial.print("t");
+    return "t";
   }
   else if (result == -27541)
   {
-    Serial.print("u");
+    return "u";
   }
   else if (result == 3825)
   {
-    Serial.print("v");
+    return "v";
   }
   else if (result == -24481)
   {
-    Serial.print("w");
+    return "w";
   }
   else if (result == -18871)
   {
-    Serial.print("x");
+    return "x";
   }
   else if (result == -4081)
   {
-    Serial.print("y");
+    return "y";
   }
   else if (result == 551499465)
   {
-    Serial.print("z");
+    return "z";
   }
   else if (result == 13260)
   {
-    Serial.print(" ");
+    return " ";
   }
   else if (result == -14536)
   {
-    Serial.print("\n");
+    return "<ENTER>";
   }
   else if (result == 18360)
   {
-    Serial.print("<DELETE>");
+    return "<DELETE>";
   }
+  return "";
 }
   
