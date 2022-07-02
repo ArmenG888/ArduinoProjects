6c369471b1c4a376437ef7405a15e66c17d42b31
ArmenG <armeng2007@gmail.com>
2022-07-02 09:53:11 -0700

    Added keyboard

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,8 @@
 #include <IRremote.hpp>
 #include <EEPROM.h>
+
+
+
 int RECV_PIN = 11;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
@@ -53,7 +56,8 @@ void loop()
 {
   if (irrecv.decode(&results))
   {
-    Serial.println(results.value, HEX);
+    ReadKeyboard(results.value);
+    //Serial.println(results.value);
     irrecv.resume(); // Receive the next value
     digitalWrite(2, HIGH); 
     delay(100); 
@@ -84,5 +88,125 @@ void Erase()
   for (int i = 0; i < 1024; i++)
   {
     EEPROM.write(i, 255);
+  }
 }
+void ReadKeyboard(int result)
+{
+  if (result == -9691)
+  {
+    Serial.print("a");
+  }
+  else if (result == -20911)
+  {
+    Serial.print("b");
+  }
+  else if (result == 30345)
+  {
+    Serial.print("c");
+  }
+  else if (result == -17851)
+  {
+    Serial.print("d");
+  }
+  else if (result == 24735)
+  {
+    Serial.print("e");
+  }
+  else if (result == 31365)
+  {
+    Serial.print("f");
+  }
+  else if (result == -1531)
+  {
+    Serial.print("g");
+  }
+  else if (result == 5865)
+  {
+    Serial.print("h");
+  }
+  else if (result == -5611)
+  {
+    Serial.print("i");
+  }
+  else if (result == -27031)
+  {
+    Serial.print("j");
+  }
+  else if (result == 22185)
+  {
+    Serial.print("k");
+  }
+  else if (result == -10711)
+  {
+    Serial.print("l");
+  }
+  else if (result == 7905)
+  {
+    Serial.print("m");
+  }
+  else if (result == 28305)
+  {
+    Serial.print("n");
+  }
+  else if (result == 6885)
+  {
+    Serial.print("o");
+  }
+  else if (result == -26011)
+  {
+    Serial.print("p");
+  }
+  else if (result == 8415)
+  {
+    Serial.print("q");
+  }
+  else if (result == 12495)
+  {
+    Serial.print("r");
+  }
+  else if (result == 15045)
+  {
+    Serial.print("s");
+  }
+  else if (result == -20401)
+  {
+    Serial.print("t");
+  }
+  else if (result == -27541)
+  {
+    Serial.print("u");
+  }
+  else if (result == 3825)
+  {
+    Serial.print("v");
+  }
+  else if (result == -24481)
+  {
+    Serial.print("w");
+  }
+  else if (result == -18871)
+  {
+    Serial.print("x");
+  }
+  else if (result == -4081)
+  {
+    Serial.print("y");
+  }
+  else if (result == 551499465)
+  {
+    Serial.print("z");
+  }
+  else if (result == 13260)
+  {
+    Serial.print(" ");
+  }
+  else if (result == -14536)
+  {
+    Serial.print("\n");
+  }
+  else if (result == 18360)
+  {
+    Serial.print("<DELETE>");
+  }
 }
+  
