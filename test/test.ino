44f3be2a72eaac7325832f7967de9855f8b1b2f4
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2023-06-18 20:27:28 -0700

    eeprom test

--- test/test.ino
+++ test/test.ino
@@ -1,43 +1,25 @@
-#include <Wire.h>    
- 
-#define disk1 0x50    //Address of 24LC256 eeprom chip
- 
-void setup(void)
+#include <EEPROM.h>
+
+void setup()
 {
   Serial.begin(9600);
-  Wire.begin();  
- 
-  unsigned int address = 0;
- 
-  writeEEPROM(disk1, address, 123);
-  Serial.print(readEEPROM(disk1, address), DEC);
+  
+  for (int i = 0; i < 255; i++)
+  {
+    EEPROM.write(i, 0);
+  }
+  int x = 0;
+  for (int i = 0; i < 1024; i++)
+  {
+    int data = EEPROM.read(i);
+    if(data != 255){
+      Serial.println(data, DEC);
+      x += 1;
+    }
+    Serial.print(x);
+    Serial.print("/1024b");
+    
+  }
+    
 }
- 
 void loop(){}
- 
-void writeEEPROM(int deviceaddress, unsigned int eeaddress, byte data ) 
-{
-  Wire.beginTransmission(deviceaddress);
-  Wire.write((int)(eeaddress >> 8));   // MSB
-  Wire.write((int)(eeaddress & 0xFF)); // LSB
-  Wire.write(data);
-  Wire.endTransmission();
- 
-  delay(5);
-}
- 
-byte readEEPROM(int deviceaddress, unsigned int eeaddress ) 
-{
-  byte rdata = 0xFF;
- 
-  Wire.beginTransmission(deviceaddress);
-  Wire.write((int)(eeaddress >> 8));   // MSB
-  Wire.write((int)(eeaddress & 0xFF)); // LSB
-  Wire.endTransmission();
- 
-  Wire.requestFrom(deviceaddress,1);
- 
-  if (Wire.available()) rdata = Wire.read();
- 
-  return rdata;
-}
